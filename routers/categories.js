6bb29ecc212876a77ea1b920e8c3b4334b4fd413
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2023-02-10 22:42:43 +0100

    added GET And PUT endpoints for category data

--- routers/categories.js
+++ routers/categories.js
@@ -16,6 +16,64 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get category by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(parseInt(id))) {
+    return res.status(400).send({ message: "category id is not a number" });
+  }
+  try {
+    const categoryData = await Category.findByPk(id);
+
+    if (categoryData === null) {
+      return res.status(404).send({ message: "Category not found" });
+    }
+    const category = {
+      name: categoryData.name,
+      quote: categoryData.quote,
+      description: categoryData.description,
+      imageUrl: categoryData.imageUrl,
+    };
+    res.status(200).send({ message: "ok", category });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { quote, description } = req.body;
+  const { id } = req.params;
+
+  if (isNaN(parseInt(id))) {
+    return res.status(400).send({ message: "Blog id is not a number" });
+  }
+
+  if (!quote || !description) {
+    return res.status(400).send({
+      message:
+        "To edit you need to specify a quote and a description for the category",
+    });
+  }
+  try {
+    const category = await Category.findByPk(id);
+    if (!category) {
+      return res.status(404).send({ message: "Can't find that category" });
+    }
+
+    await category.update({
+      quote,
+      description,
+    });
+
+    res.status(200).send({ message: "ok", category });
+  } catch (error) {
+    console.log(error);
+    return res.status(503).send({ message: "Something went wrong, sorry" });
+  }
+});
+
 //Get all blogs belonging to a category
 
 router.get("/:id/blogs", async (req, res) => {
